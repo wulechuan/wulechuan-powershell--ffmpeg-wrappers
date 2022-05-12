36a4ee7f018f1d94c9b753c57a8805a2955add7e
吴乐川 <wulechuan@live.com>
2022-05-12 19:45:13 +0800

    对接了新版的集得工具

--- 用于研发阶段的命令行工具/Add-添加所有集得源.ps1
+++ 用于研发阶段的命令行工具/Add-添加所有集得源.ps1
@@ -1,26 +1,8 @@
-#
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：码云'     -特征颜色 'DarkRed' `
+    -集得源之完整地址 'git@gitee.com:nanchang-wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
 
-Write-Host
-Write-Host -F 'DarkRed' '===== 添加【集得】源： 吴乐川：码云  ========================='
-Write-Host
-git  remote  add  '吴乐川：码云'  'git@gitee.com:nanchang-wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
-Write-Host
-Write-Host
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：阿里云'   -特征颜色 'Blue' `
+    -集得源之完整地址    'git@code.aliyun.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
 
-
-
-Write-Host
-Write-Host -F 'DarkGreen' '===== 添加【集得】源： 吴乐川：阿里云  ========================='
-Write-Host
-git  remote  add  '吴乐川：阿里云'  'git@code.aliyun.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
-Write-Host
-Write-Host
-
-
-
-Write-Host
-Write-Host -F 'Yellow' '===== 添加【集得】源： 吴乐川：GitHub  ========================='
-Write-Host
-git  remote  add  '吴乐川：GitHub'  'git@github.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
-Write-Host
-Write-Host
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：GitHub'  -特征颜色 'Yellow' `
+    -集得源之完整地址         'git@github.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'

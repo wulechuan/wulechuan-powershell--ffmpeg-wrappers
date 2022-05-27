3d5644327271b05370b7d71647090d7e9070171b
吴乐川 <wulechuan@live.com>
2022-05-27 15:36:07 +0800

    无关痛痒

--- 用于研发阶段的命令行工具/Add-添加所有集得源.ps1
+++ 用于研发阶段的命令行工具/Add-添加所有集得源.ps1
@@ -1,8 +1,8 @@
-Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：码云'     -特征颜色 'DarkRed' `
-    -集得源之完整地址 'git@gitee.com:nanchang-wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：码云' `
+    -特征颜色 'DarkRed'      -集得源之完整地址 'git@gitee.com:nanchang-wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
 
-Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：阿里云'   -特征颜色 'Blue' `
-    -集得源之完整地址    'git@code.aliyun.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：阿里云' `
+    -特征颜色 'Blue'         -集得源之完整地址    'git@code.aliyun.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
 
-Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：GitHub'  -特征颜色 'Yellow' `
-    -集得源之完整地址         'git@github.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
+Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：GitHub' `
+    -特征颜色 'Yellow'       -集得源之完整地址         'git@github.com:wulechuan/wulechuan-powershell--ffmpeg-wrappers.git'
